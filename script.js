6f59a8975be5c12d76578cad639584a62e8c3d79
curmemn <curmemn@gmail.com>
2024-11-19 12:08:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// 현재 월, 연도 초기화
+let currentYear = new Date().getFullYear();
+let currentMonth = new Date().getMonth() + 1;
+
+// 달력 만드는 함수
+
+function createCalendar(year, month){
+  const today = new Date();
+  // 달력의 첫째날
+  const firstDay = new Date(year, month - 1, 1);
+  // 달력의 마지막날
+  const lastDay = new Date(year, month, 0);
+  // 달력 한줄씩 만들기
+  let html = "<tr>";
+
+  //달력 헤더에 현재 월과 연도 표시
+  let CalendarHeader = document.getElementById('current');
+  CalendarHeader.innerText = `${year}년 ${month}월`
+
+  //첫주 빈칸 채우기
+  for(let i = 0; i < firstDay.getDay(); i++){
+    html += "<td></td>";
+  }
+  //날짜 채우기
+  for(let day = 1; day <= lastDay.getDate(); day++){
+    const currentDay = new Date(year, month -1, day).getDay();
+    document.querySelectorAll('tr').forEach
+    html += `<td>${day}</td>`;
+    if(currentDay === 6){
+      html += "</tr><tr>";
+    }
+  }
+  // 마지막 주 빈 칸 채우기
+  const lastDayOfWeek = lastDay.getDay();
+  for (let i = lastDayOfWeek + 1; i <= 6; i++) {
+    html += "<td></td>";
+  }
+  html += "</tr>";
+  document.getElementById("calendarBody").innerHTML = html;
+}
+
+//이전달 다음달 함수
+function changeMonth(offset){
+  currentMonth += offset;
+  if(currentMonth > 12){
+    currentMonth = 1;
+    currentYear++;
+  }else if(currentMonth <1){
+    currentMonth = 12;
+    currentYear--;
+  }
+  createCalendar(currentYear, currentMonth);
+}
+
+createCalendar(currentYear, currentMonth);
+
+
+/* let clickedDate = '';
+//클릭한 날짜 표시 함수
+function clickDate(year, month, date){
+  clickedDate = new Date(year, month - 1 , date);
+
+} */
